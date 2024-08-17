0a59699114d587f004097ed9a3210c5d59e62d3e
Lachlan Stewart <lachlanstewart6@gmail.com>
2024-08-17 21:32:50 +1000

    Init

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Actor : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     int currentHealth;
     public int maxHealth;
 
+    private GameObject camera;
+
     void Awake()
     {
+       // camera = GameObject.FindGameObjectWithTag("Camera");
         currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform);
     }
 
     public void TakeDamage(int amount)
